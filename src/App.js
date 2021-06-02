1a3bf905b3e723829826c1c9f60dc292caabfe9c
yzjy19 <56280386+yzjy19@users.noreply.github.com>
2021-06-02 12:52:37 -0400

    Products setup using Commerce.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
 import { Products, Navbar } from './components';
+import { commerce } from './lib/shop';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async (req, res) => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    console.log(products);
     return (
         <div>
             <Navbar />
-            <Products />
+            <Products products={products} />
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
+import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', image: 'https://www.brooksrunning.com/dw/image/v2/aaev_prd/on/demandware.static/-/Sites-BrooksCatalog/default/dw501fef3d/images/ProductImages/110324/110324_434_l_WR.jpg?sw=900', description: 'Running shoes', price: '$10'},
-    {id: 2, name: 'IPad', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmRY7OzpCf_O8aztbKv05Yb0IH8b0GPZehSw&usqp=CAU', description: 'Apple IPad', price: '$100'}
-]
 
-const Products = () => {
+
+const Products = ({ products }) => {
+    const classes = useStyles();
     return (
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
